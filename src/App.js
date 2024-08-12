da58e052a0f2206ae04d8ec9296e3129623dd007
maxi <maxbrax144@gmail.com>
2024-08-12 12:15:27 -0300

    Agregue vinculos navbar y Sobre mí

--- src/App.js
+++ src/App.js
@@ -15,11 +15,21 @@ function App() {
         <h2>Tecnico Informatico</h2>
       </header>
       
-      <div className='App-header' id='home'>
+      <div className='App-header' id='Portfolio'>
         <h1>Portfolio</h1>
       <ControlledCarousel></ControlledCarousel>
       </div>
-      <Form></Form>
+      <section3 className='App-header' id='Sobremí' >
+      <h1>Sobre mí</h1>
+      <h2>Soy Maximiliano, tengo 18 años y siempre he sido apasionado por la ciencia y
+                la tecnología. Desde pequeño, me ha fascinado entender cómo funcionan las cosas
+                a mi alrededor, desarmando y reconstruyendo aparatos electrónicos en mi casa. En
+                el colegio, destaco en materias como matemáticas y física, y disfruto de los
+                desafíos y problemas que estas asignaturas me presentan. Soy una persona curiosa
+                y creativa, siempre buscando nuevas soluciones y mejoras para problemas
+                cotidianos.</h2>
+      </section3>
+      <Form id='Form'></Form>
       <Footer></Footer>
     </div>
   );
--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -5,14 +5,14 @@ import Navbar from 'react-bootstrap/Navbar';
 function ColorSchemesExample() {
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar bg="dark" data-bs-theme="dark" fixed='top'>
         <Container>
           <Navbar.Brand href="#home">Maximiliano Jimenez</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#Portfolio">Porfolio</Nav.Link>
+            <Nav.Link href="#Sobremí">Sobre mí</Nav.Link>
+            <Nav.Link href="#Form">Contacto</Nav.Link>
+            
           </Nav>
         </Container>
       </Navbar>
